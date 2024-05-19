9038ae51af0681a29f7ca9ca621368ee8d7187f3
Ali Abudal <abudalali35@gmail.com>
2024-05-19 19:19:43 +0300

    feat: configure shadcn ui

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,45 +1,10 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Input } from "~/components/ui/input";
 
 export const meta: MetaFunction = () => {
-    return [
-        { title: "New Remix App" },
-        { name: "description", content: "Welcome to Remix!" },
-    ];
+    return [{ title: "Remix Template Starter" }];
 };
 
 export default function Index() {
-    return (
-        <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-            <h1>Welcome to Remix</h1>
-            <ul>
-                <li>
-                    <a
-                        target="_blank"
-                        href="https://remix.run/tutorials/blog"
-                        rel="noreferrer"
-                    >
-                        15m Quickstart Blog Tutorial
-                    </a>
-                </li>
-                <li>
-                    <a
-                        target="_blank"
-                        href="https://remix.run/tutorials/jokes"
-                        rel="noreferrer"
-                    >
-                        Deep Dive Jokes App Tutorial
-                    </a>
-                </li>
-                <li>
-                    <a
-                        target="_blank"
-                        href="https://remix.run/docs"
-                        rel="noreferrer"
-                    >
-                        Remix Docs
-                    </a>
-                </li>
-            </ul>
-        </div>
-    );
+    return <Input type="email" placeholder="Email" />;
 }
