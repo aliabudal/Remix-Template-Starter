e68907116767228ebeb58e031ca31f7803d42a7c
Ali Abudal <abudalali35@gmail.com>
2024-06-12 04:40:42 +0300

    fix: add missing files

--- app/lib/styles.ts
+++ app/lib/styles.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+export function themeBorder(...inputs: ClassValue[]) {
+	return cn(
+		"overflow-hidden rounded-lg shadow-md",
+		"bg-[color:var(--theme-light,hsl(var(--zinc-100)))] dark:bg-[color:var(--theme-dark,hsl(var(--zinc-800-dark)))]",
+		"border border-[color:var(--theme-light-border,hsl(var(--zinc-300)))] dark:border-[color:var(--theme-dark-border,hsl(var(--zinc-700-dark)))]",
+		"p-6",
+		...inputs
+	);
+}
