26280d595e14b9304f98419fc97a2c040c629f27
Ali Abudal <73780467+aliabudal@users.noreply.github.com>
2024-06-12 04:43:50 +0300

    fix: add missing files (#14)
    * feat: better header, link to products page
    
    header is now sticky and has a blur effect when scrolling
    
    * fix: footer logo, card hover coloring
    
    * feat: add payment methods and link to our plans
    
    * fix: remove redundant checks
    
    * feat: add new product, pagination
    
    * feat: edit and delete products
    
    * fix: add missing files

--- app/lib/products.server.ts
+++ app/lib/products.server.ts
@@ -22,8 +22,8 @@ export async function getAllProducts(
 }
 
 export async function getProductsCount({ DB }: AppLoadContext) {
-    return await DB.select({ count: count() }).from(product)
-  }
+	return await DB.select({ count: count() }).from(product);
+}
 
 export async function createProduct(
 	{ DB }: AppLoadContext,
@@ -42,4 +42,31 @@ export async function createProduct(
 
 export async function deleteProduct({ DB }: AppLoadContext, productId: string) {
 	return await DB.delete(product).where(eq(product.id, productId));
+}
+
+export async function getProductById(
+	{ DB }: AppLoadContext,
+	productId: string
+) {
+	return await DB.query.product.findFirst({
+		where: eq(product.id, productId),
+	});
+}
+
+export async function updateProduct(
+	{ DB }: AppLoadContext,
+	productData: {
+		id: string;
+		name: string;
+		description: string;
+		price: number;
+	}
+) {
+	return await DB.update(product)
+		.set({
+			name: productData.name,
+			description: productData.description,
+			price: productData.price,
+		})
+		.where(eq(product.id, productData.id));
 }
